3b656b23842668e8d6465bdc6f49b7edb0020e02
cannell001 <cannell001@gannon.edu>
2018-11-06 13:34:50 -0500

    Display Alien Data from MongoDB

---
+++ imports/api/aliens.js
@@ -0,0 +1,3 @@
+import {Mongo} from 'meteor/mongo';
+
+export const Aliens = new Mongo.Collection('aliens');
--- server/main.js
+++ server/main.js
@@ -1,10 +1,15 @@
 import {Meteor} from 'meteor/meteor';
-import {Players} from './../imports/api/players';
+import {Aliens} from './../imports/api/aliens';
 
 Meteor.startup(function () {
-  Players.insert({
-    name: 'Vikram',
-    score: 99
+  Aliens.insert({
+      firstName: "John",
+      lastName: "Coffman",
+      email: "coff@gmail.com",
+      longAgo: "6 Months",
+      duration: "3 months",
+      experience: "tubes",
+      color: "green"
   });
-  console.log(Players.find().fetch());
+  console.log(Aliens.find().fetch());
 });
